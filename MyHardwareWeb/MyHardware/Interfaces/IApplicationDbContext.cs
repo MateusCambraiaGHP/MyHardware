13aa235f83293781a5cd4e0790b473a6cf9ce140
Mateus.Cambraia <mateuscambraia.0246@aluno.una.br>
2022-08-02 10:28:57 -0300

    #1

---
+++ MyHardwareWeb/MyHardware/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace MyHardware.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Supplier_Product> Supplier_Product { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Contact> Contact { get; set; }
+        public DbSet<Supplier> Supplier { get; set; }
+        public int Save();
+    }
+}
